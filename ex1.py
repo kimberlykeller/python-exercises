fdc5e26987f05c4d11eb1dba5b78f4eecf35e7f8
Kimberly Keller <keller.kimberly@gmail.com>
2016-12-30 03:12:50 +0000

    first python exercise

---
+++ ex1.py
@@ -0,0 +1,12 @@
+#exercise 1 in Learn Python the Hard Way
+
+list = ["Hello World!", "Hello again!", "I like typing this.", "This is fun.", "Yay! Printing.", "I'd much rather that you 'not'.", 'I "said" do not touch this.']
+
+print list[2]
+print "Hello World!"
+print "Hello again!"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather that you 'not'."
+print 'I "said" do not touch this.'
